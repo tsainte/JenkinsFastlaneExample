8a8c9b4f2474c86d39cd5cd8dfdce24755e504eb
Tiago Bencardino <tiagobencardino@gmail.com>
2018-04-09 14:28:30 +0100

    adding slather

---
+++ JenkinsFastlaneExample/Calculator.swift
@@ -0,0 +1,9 @@
+//
+//  Calculator.swift
+//  JenkinsFastlaneExample
+//
+//  Created by Tiago Bencardino on 09/04/2018.
+//  Copyright © 2018 Christoph Herrmann. All rights reserved.
+//
+
+import Foundation
